ed4904244d4bf800658a94385a76e018e87b6936
rynst <rsteubs@yahoo.com>
2022-08-19 08:06:15 -0700

    feat: add in web3 react

--- tailwindui-salient/src/pages/register.jsx
+++ tailwindui-salient/src/pages/register.jsx
@@ -1,16 +1,20 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import { useWeb3React } from '@web3-react/core'
 
 import { AuthLayout } from '@/components/AuthLayout'
 import { Button } from '@/components/Button'
 import { SelectField, TextField } from '@/components/Fields'
 import { Logo } from '@/components/Logo'
+import { CoinbaseWallet, Injected, WalletConnect } from '@/config/connectors'
 
 export default function Register() {
+  const { activate, deactivate } = useWeb3React();
+
   return (
     <>
       <Head>
-        <title>Sign Up - TaxPal</title>
+        <title>Sign Up - Salamat</title>
       </Head>
       <AuthLayout>
         <div className="flex flex-col">
@@ -95,6 +99,37 @@ export default function Register() {
             </Button>
           </div>
         </form>
+        <div className="mt-6 relative">
+          <div className="absolute inset-0 flex items-center" aria-hidden="true">
+            <div className="w-full border-t border-gray-300" />
+          </div>
+          <div className="relative flex justify-center text-sm">
+            <span className="px-2 bg-white text-gray-500">Or continue with</span>
+          </div>
+        </div>
+        <div className="col-span-full">
+          <button
+            onClick={() => { activate(Injected) }}
+            type="button"
+            className="mt-6 w-full inline-flex justify-center items-center px-4 py-2 border border-gray-300 shadow-sm text-base font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          >
+            Login with Metamask
+          </button>
+          <button
+            onClick={() => { activate(CoinbaseWallet) }}
+            type="button"
+            className="mt-6 w-full inline-flex justify-center items-center px-4 py-2 border border-gray-300 shadow-sm text-base font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          >
+            Login with Coinbase
+          </button>
+          <button
+            onClick={() => { activate(WalletConnect) }}
+            type="button"
+            className="mt-6 w-full inline-flex justify-center items-center px-4 py-2 border border-gray-300 shadow-sm text-base font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          >
+            Login with WalletConnect
+          </button>
+        </div>
       </AuthLayout>
     </>
   )
